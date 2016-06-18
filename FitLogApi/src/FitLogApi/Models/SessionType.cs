9b87e5b6af6bce0e6c5a784f76ffa8fa70a77dfa
Matt Qualls <mattqualls@comcast.net>
2016-06-18 18:46:45 -0500

    added sessiontype properties and key relationships

--- FitLogApi/src/FitLogApi/Models/SessionType.cs
+++ FitLogApi/src/FitLogApi/Models/SessionType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace FitLogApi.Models
 {
     public class SessionType
     {
+        [Key]
+        public int SessionTypeId { get; set; }
+        public int SessionId { get; set; }
+        public bool Treadmill { get; set; }
+        public bool Bike { get; set; }
+        public bool Elliptical { get; set; }
+
+        public Session Session { get; set; }
     }
 }
