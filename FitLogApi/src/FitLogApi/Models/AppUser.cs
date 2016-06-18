463590c27e97bb0d5a9094a3a86f2ec3afa52873
Matt Qualls <mattqualls@comcast.net>
2016-06-18 18:40:38 -0500

    added appUser properties along with appropriate primary,foreign key relationships declared

--- FitLogApi/src/FitLogApi/Models/AppUser.cs
+++ FitLogApi/src/FitLogApi/Models/AppUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,15 @@ namespace FitLogApi.Models
 {
     public class AppUser
     {
+        [Key]
+        public int ApplicationUserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string password { get; set; }
+        public int InitialWeight { get; set; }
+        public int HeightInInches { get; set; }
+        public int Age { get; set; }
+
+        public ICollection<Session> Sessions { get; set; }
     }
 }
