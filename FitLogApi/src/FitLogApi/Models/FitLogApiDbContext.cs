49e3aa665d1281929c48e28c8fe4da6e52f2c1c7
Matt Qualls <mattqualls@comcast.net>
2016-06-18 18:51:09 -0500

    added DbContext with appropriate DbSet's for models

---
+++ FitLogApi/src/FitLogApi/Models/FitLogApiDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitLogApi.Models
+{
+    public class FitLogApiDbContext : DbContext
+    {
+        public FitLogApiDbContext(DbContextOptions<FitLogApiDbContext> options)
+      : base(options)
+    { }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<SessionType> SessionTypes { get; set; }
+    }
+}
