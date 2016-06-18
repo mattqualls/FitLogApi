46fc8807ceadd4e97fc77b80e9831a3241e9ff25
Matt Qualls <mattqualls@comcast.net>
2016-06-18 18:56:44 -0500

    added proper using statements in Startup.cs and used AddDbContext method in ConfigureServices so that I can use NuGet Package Manager to create database with migrations

--- FitLogApi/src/FitLogApi/Startup.cs
+++ FitLogApi/src/FitLogApi/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using FitLogApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FitLogApi
 {
@@ -27,6 +29,8 @@ namespace FitLogApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=FitLogApi;Trusted_Connection=True;";
+            services.AddDbContext<FitLogApiDbContext>(options => options.UseSqlServer(connection));
             // Add framework services.
             services.AddMvc();
         }
