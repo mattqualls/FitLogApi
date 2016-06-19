c96a1e98106fee0afa5b3fc51cf2740f093b97c3
Matt Qualls <mattqualls@comcast.net>
2016-06-18 19:01:32 -0500

    created database via Add-Migration MyFirstMigration/Update-Database. Creation successful

---
+++ FitLogApi/src/FitLogApi/Migrations/20160618235808_MyFirstMigration.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FitLogApi.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppUsers",
+                columns: table => new
+                {
+                    AppUserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Age = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    HeightInInches = table.Column<int>(nullable: false),
+                    InitialWeight = table.Column<int>(nullable: false),
+                    Username = table.Column<string>(nullable: true),
+                    password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUsers", x => x.AppUserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    SessionId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AppUserId = table.Column<int>(nullable: true),
+                    ApplUserId = table.Column<int>(nullable: false),
+                    CaloriesBurned = table.Column<int>(nullable: false),
+                    MilesTraveled = table.Column<int>(nullable: false),
+                    MinutesWorked = table.Column<int>(nullable: false),
+                    SessionDate = table.Column<DateTime>(nullable: false),
+                    SessionWeight = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.SessionId);
+                    table.ForeignKey(
+                        name: "FK_Sessions_AppUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AppUsers",
+                        principalColumn: "AppUserId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SessionTypes",
+                columns: table => new
+                {
+                    SessionTypeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Bike = table.Column<bool>(nullable: false),
+                    Elliptical = table.Column<bool>(nullable: false),
+                    SessionId = table.Column<int>(nullable: false),
+                    Treadmill = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SessionTypes", x => x.SessionTypeId);
+                    table.ForeignKey(
+                        name: "FK_SessionTypes_Sessions_SessionId",
+                        column: x => x.SessionId,
+                        principalTable: "Sessions",
+                        principalColumn: "SessionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_AppUserId",
+                table: "Sessions",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SessionTypes_SessionId",
+                table: "SessionTypes",
+                column: "SessionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SessionTypes");
+
+            migrationBuilder.DropTable(
+                name: "Sessions");
+
+            migrationBuilder.DropTable(
+                name: "AppUsers");
+        }
+    }
+}
---
+++ FitLogApi/src/FitLogApi/Migrations/FitLogApiDbContextModelSnapshot.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using FitLogApi.Models;
+
+namespace FitLogApi.Migrations
+{
+    [DbContext(typeof(FitLogApiDbContext))]
+    partial class FitLogApiDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rc2-20901")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("FitLogApi.Models.AppUser", b =>
+                {
+                    b.Property<int>("AppUserId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Age");
+
+                    b.Property<string>("Email");
+
+                    b.Property<int>("HeightInInches");
+
+                    b.Property<int>("InitialWeight");
+
+                    b.Property<string>("Username");
+
+                    b.Property<string>("password");
+
+                    b.HasKey("AppUserId");
+
+                    b.ToTable("AppUsers");
+                });
+
+            modelBuilder.Entity("FitLogApi.Models.Session", b =>
+                {
+                    b.Property<int>("SessionId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int?>("AppUserId");
+
+                    b.Property<int>("ApplUserId");
+
+                    b.Property<int>("CaloriesBurned");
+
+                    b.Property<int>("MilesTraveled");
+
+                    b.Property<int>("MinutesWorked");
+
+                    b.Property<DateTime>("SessionDate");
+
+                    b.Property<int>("SessionWeight");
+
+                    b.HasKey("SessionId");
+
+                    b.HasIndex("AppUserId");
+
+                    b.ToTable("Sessions");
+                });
+
+            modelBuilder.Entity("FitLogApi.Models.SessionType", b =>
+                {
+                    b.Property<int>("SessionTypeId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<bool>("Bike");
+
+                    b.Property<bool>("Elliptical");
+
+                    b.Property<int>("SessionId");
+
+                    b.Property<bool>("Treadmill");
+
+                    b.HasKey("SessionTypeId");
+
+                    b.HasIndex("SessionId");
+
+                    b.ToTable("SessionTypes");
+                });
+
+            modelBuilder.Entity("FitLogApi.Models.Session", b =>
+                {
+                    b.HasOne("FitLogApi.Models.AppUser")
+                        .WithMany()
+                        .HasForeignKey("AppUserId");
+                });
+
+            modelBuilder.Entity("FitLogApi.Models.SessionType", b =>
+                {
+                    b.HasOne("FitLogApi.Models.Session")
+                        .WithMany()
+                        .HasForeignKey("SessionId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
